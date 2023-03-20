d0b96dc36ee98bbdd30bd303ea506d079b84fc6f
Alejandra Landa <aleriquene@gmail.com>
2023-03-20 16:50:03 +0000

    Paper created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+def homepage
+  render({ :template=>"game_template/rules.html.erb"})
+end
 def play_rock
   # redirect_to("") will redirect to any url you put in there
   render({:template => "game_template/user_rock.html.erb"})
@@ -10,5 +13,18 @@ def play_rock
   #:template=>
   #.html_safe ruby saving us from ppl injecting bad html
   #.erb is an embedded ruby template
+end
+def play_paper
+ @comp_move = ["rock", "paper", "scissors"].sample
+
+   if @comp_move == "rock" 
+    @outcome = "won"
+   elsif @comp_move == "paper" 
+    @outcome = "tied"
+  elsif @omp_move == "scissors" 
+    @outcome = "lost"
+   end
+
+  render({:template => "game_template/user_paper.html.erb"})
 end
 end
