d0f85bcbd5bd7bd902481c60917be96c2da96b07
Alejandra Landa <aleriquene@gmail.com>
2023-03-20 15:48:05 +0000

    Rock random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+def play_rock
+  # redirect_to("") will redirect to any url you put in there
+  render({:template => "game_template/user_rock.html.erb"})
 
+  #:template=>
+  #.html_safe ruby saving us from ppl injecting bad html
+  #.erb is an embedded ruby template
+end
 end
